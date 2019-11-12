eb5363177604b5df3f66a1350ab267f8bd1cb861
Robert <kraje.robert@gmail.com>
2019-11-13 00:53:44 +0300

    v2

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,40 +1,52 @@
 <template>
-  <section class="form-simple">
-    <mdb-row> 
-      <mdb-col md="5">
-        <mdb-card>
-          <div class="header pt-3 grey lighten-2">
-            <mdb-row class="d-flex justify-content-start">
-              <h3 class="deep-grey-text mt-3 mb-4 pb-1 mx-5">Log in</h3>
-            </mdb-row>
-          </div>
-          <mdb-card-body class="mx-4 mt-4">
-            <mdb-input label="Email" type="text"/>
-            <mdb-input label="Password" type="password" containerClass="mb-0"/>
-            <p class="font-small grey-text d-flex justify-content-end">Forgot <a href="#" class="dark-grey-text font-weight-bold ml-1"> Password?</a></p>
-            <div class="text-center mb-4 mt-5">
-              <mdb-btn color="danger" type="button" class="btn-block z-depth-2">Log in</mdb-btn>
-            </div>
-            <p class="font-small grey-text d-flex justify-content-center">Don't have an account? <a href="#/register" class="dark-grey-text font-weight-bold ml-1"> Sign up</a></p>
-          </mdb-card-body>
-        </mdb-card>
-      </mdb-col>
-    </mdb-row>
-  </section>
+   <div div="signin">
+       <div class="login-form">
+           <b-card
+             title="Signin"
+             tag="article"
+             style="max-width: 20rem;"
+             class="mb-2"
+           >
+           <div>
+               <b-alert
+                     :show="dismissCountDown"
+                     dismissible
+                     variant="danger"
+                     @dismissed="dismissCountDown=0"
+                     @dismiss-count-down="countDownChanged"
+                   > {{ alertMessage }}
+                   </b-alert>
+           </div>
+             <div>
+                <b-form-input type="text" placeholder="Email" v-model="email" />
+                <div class="mt-2"></div>
+
+                <b-form-input type="password" placeholder="Password" v-model="password" />
+                <div class="mt-2"></div>
+             </div>
+
+             <b-button v-on:click="login" variant="primary">Login</b-button>
+
+             <hr class="my-4" />
+
+             <b-button variant="link">Forget password?</b-button>
+           </b-card>
+         </div>
+   </div>
 </template>
 
 <script>
-  import { mdbRow, mdbCol, mdbCard, mdbCardBody, mdbInput, mdbBtn } from 'mdbvue';
+  //import { mdbRow, mdbCol, mdbCard, mdbCardBody, mdbInput, mdbBtn } from 'mdbvue';
   import axios from 'axios'
 
   export default {
     data(){
         return{
-          email: '',
-          password: ''
+          email: 'mail4@mail.ru',
+          password: '12345'
         }
     },
-    name: 'FormsPage',
+    /*name: 'FormsPage',
     components: {
       mdbRow,
       mdbCol,
@@ -42,16 +54,16 @@
       mdbCardBody,
       mdbInput,
       mdbBtn
-    },
+    },*/
     methods: {
         login(){
-          axios.post('/login')
-          .then(request =>{
-            this.$data.email = request.data.content;
-            this.$data.password = request.data.content;
+          axios.post('http://localhost:9000/login', {'email': this.$data.email, 'password': this.$data.password})
+          .then(response =>{
+            console.log(response);
+            this.$router.push('/register')
           })
           .catch(error => {
-            console.log('ERROR: '+error.response.data);
+            console.log(error);
           })
         }
     }
@@ -59,26 +71,8 @@
 </script>
 
 <style>
-  .form-simple .font-small {
-    font-size: 0.8rem; }
-
-  .form-simple .header {
-    border-top-left-radius: .3rem;
-    border-top-right-radius: .3rem; }
-
-  .form-simple input[type=text]:focus:not([readonly]) {
-    border-bottom: 1px solid #ff3547;
-    -webkit-box-shadow: 0 1px 0 0 #ff3547;
-    box-shadow: 0 1px 0 0 #ff3547; }
-
-  .form-simple input[type=text]:focus:not([readonly]) + label {
-    color: #4f4f4f; }
-
-  .form-simple input[type=password]:focus:not([readonly]) {
-    border-bottom: 1px solid #ff3547;
-    -webkit-box-shadow: 0 1px 0 0 #ff3547;
-    box-shadow: 0 1px 0 0 #ff3547; }
-
-  .form-simple input[type=password]:focus:not([readonly]) + label {
-    color: #4f4f4f; }
+.login-form {
+   margin-left: 38%;
+   margin-top: 50px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/lib/css/mdb.min.css'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import Vue from 'vue'
 import App from './App.vue'
@@ -8,6 +11,7 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue) 
 
 new Vue({
   router,
